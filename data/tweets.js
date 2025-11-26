b1304963edd89c244c3a05ca20cdcf1380d203d3
ridwan waheed <waheedridwan96@gmail.com>
2025-11-27 00:45:37 +0100

    Separate sample data from real user data
    Changes:
    - Move real tweets.js to my_data/ folder (gitignored)
    - Create data/ folder with sample tweets.js for testing
    - Update .gitignore to ignore my_data/ instead of data/
    - data/ folder now tracked in git for easy setup
    
    To use real data:
      Update application.properties to point to my_data/tweets.js

---
+++ data/tweets.js
@@ -0,0 +1,37 @@
+window.YTD.tweets.part0 = [
+  {
+    "tweet" : {
+      "id_str" : "1234567890",
+      "full_text" : "This is a sample tweet about coding and technology",
+      "created_at" : "Thu Nov 13 21:44:31 +0000 2024"
+    }
+  },
+  {
+    "tweet" : {
+      "id_str" : "1234567891",
+      "full_text" : "RT @someone: This is a retweet and should be filtered out",
+      "created_at" : "Thu Nov 14 10:30:00 +0000 2024"
+    }
+  },
+  {
+    "tweet" : {
+      "id_str" : "1234567892",
+      "full_text" : "Another tweet with some damn profanity that might get flagged",
+      "created_at" : "Thu Nov 15 15:20:00 +0000 2024"
+    }
+  },
+  {
+    "tweet" : {
+      "id_str" : "1234567893",
+      "full_text" : "I'm going to kill this bug! Programming can be tough sometimes",
+      "created_at" : "Thu Nov 16 09:00:00 +0000 2024"
+    }
+  },
+  {
+    "tweet" : {
+      "id_str" : "1234567894",
+      "full_text" : "Just finished a great book on software architecture. Highly recommended!",
+      "created_at" : "Thu Nov 17 18:45:00 +0000 2024"
+    }
+  }
+]
